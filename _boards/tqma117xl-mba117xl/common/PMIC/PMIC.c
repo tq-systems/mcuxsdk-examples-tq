a564c52d9d793ba67ef78f3bb0505963c537a3e7
Kevin Hänig <Kevin.Haenig@tq-group.com>
2026-06-18 14:56:19 +0200

    treewide: unified author names
    Adjusted writing to "Isaac Lucas de Lima Yuki", no further changes have been made.
    
    Signed-off-by: Kevin Hänig <Kevin.Haenig@tq-group.com>

--- _boards/tqma117xl-mba117xl/common/PMIC/PMIC.c
+++ _boards/tqma117xl-mba117xl/common/PMIC/PMIC.c
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * Copyright (c) 2021-2026 TQ-Systems GmbH <oss@ew.tq-group.com>,
  * D-82229 Seefeld, Germany.
- * Author: Isaac L. L. Yuki, Bernhard Herz
+ * Author: Isaac Lucas de Lima Yuki, Bernhard Herz
  */
 //******************************************************************************
 
--- _boards/tqma117xl-mba117xl/xip/tqma117xl-mba117xl_flexspi_nor_config.c
+++ _boards/tqma117xl-mba117xl/xip/tqma117xl-mba117xl_flexspi_nor_config.c
@@ -7,7 +7,7 @@
  *
  * Copyright (c) 2021-2026 TQ-Systems GmbH <oss@ew.tq-group.com>,
  * D-82229 Seefeld, Germany.
- * Author: Isaac L. L. Yuki
+ * Author: Isaac Lucas de Lima Yuki
  */
 //*****************************************************************************
 
