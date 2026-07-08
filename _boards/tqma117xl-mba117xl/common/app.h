cb95019cffbc2fb08053410d39d0981107eddcb7
Mohammad Tawsif Ahmad Ansari <MohammadTawsifAhmad.Ansari@tq-group.com>
2026-07-08 17:29:54 +0200

    _boards: updated tqma117xl-mba117xl CAN bus Ping Pong transfer between two boards demo
    It contains two demos, one for FlexCAN and other for CANFD
    
    Signed-off-by: Mohammad Tawsif Ahmad Ansari <MohammadTawsifAhmad.Ansari@tq-group.com>

--- _boards/tqma117xl-mba117xl/common/app.h
+++ _boards/tqma117xl-mba117xl/common/app.h
@@ -25,6 +25,36 @@
 #define EXAMPLE_SNVS_IRQHandler SNVS_HP_NON_TZ_IRQHandler
 
 /*******************************************************************************
+ * Flex CAN defines
+ ******************************************************************************/
+ 
+#define EXAMPLE_CAN                   CAN3
+#define EXAMPLE_FLEXCAN_IRQn          CAN3_IRQn
+#define EXAMPLE_FLEXCAN_RxWarningIRQn CAN3_IRQn
+#define EXAMPLE_FLEXCAN_BusOffIRQn    CAN3_IRQn
+#define EXAMPLE_FLEXCAN_ErrorIRQn     CAN3_IRQn
+#define EXAMPLE_FLEXCAN_MBIRQn        CAN3_IRQn
+#define EXAMPLE_FLEXCAN_IRQHandler    CAN3_IRQHandler
+
+#define TX_MESSAGE_BUFFER_NUM      (8)
+#define RX_MESSAGE_BUFFER_NUM      (9)
+#define RX_QUEUE_BUFFER_BASE       (1U)
+#define RX_QUEUE_BUFFER_SIZE       (4U)
+
+/* Select OSC24Mhz as master flexcan clock source */
+#define FLEXCAN_CLOCK_SOURCE_SELECT (1U)
+/* Clock divider for master flexcan clock source */
+#define FLEXCAN_CLOCK_SOURCE_DIVIDER (1U)
+/*
+ * Get frequency of flexcan clock used in FLEXCAN_FDCalculateImprovedTimingValues() function 
+ * in file flexcan_ping_pong_buffer_transfer.c that runs the demo.
+ */
+#define EXAMPLE_CAN_CLK_FREQ ((CLOCK_GetRootClockFreq(kCLOCK_Root_Can3) / 100000U) * 100000U)
+/* Set USE_IMPROVED_TIMING_CONFIG macro to use api to calculates the improved CAN / CAN FD timing values. */
+#define USE_IMPROVED_TIMING_CONFIG (1U)
+/*${macro:end}*/
+
+/*******************************************************************************
  * Prototypes
  ******************************************************************************/
 
